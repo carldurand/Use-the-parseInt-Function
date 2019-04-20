e9a2bf547095f8c80ec2d424bf594792d97eef65
Carl Durand <48800487+carldurand@users.noreply.github.com>
2019-04-20 14:46:56 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openWin()
-{
-myWindow=window.open("","","width=200,height=100");
-myWindow.document.write("<p>MUHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH!!!!!!!!'</p>");
-}
-function myFunction() {
-  document.getElementById("text").innerHTML = "HAHAHAHA I WILL RULE YOU FOREVER!!!!!!!!!!!!"
-}
